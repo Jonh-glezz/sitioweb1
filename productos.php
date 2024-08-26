093d12d222e4c816943a3c89fa5c2ce144b8de13
Jonh-glezz <jonathanjoabgomo77@gmail.com>
2024-08-25 21:58:58 -0600

    CATALOGO DE PRODUCTOS

--- productos.php
+++ productos.php
@@ -1,225 +1,79 @@
-<?php include("../template/cabecera.php"); ?>
-<?php 
-$txtID=(isset($_POST['txtID']))?$_POST['txtID']:"";
-$txtNombre=(isset($_POST['txtNombre']))?$_POST['txtNombre']:"";
-$txtImagen=(isset($_FILES['txtImagen']['name']))?$_FILES['txtImagen']['name']:"";
-$accion=(isset($_POST['accion']))?$_POST['accion']:"";
+<?php include("template/cabecera.php"); ?>
 
-include("../config/bd.php");
-
-switch($accion){
-        case"Agregar":
-            //INSERT INTO `productos` (`id`, `nombre`, `imagen`) VALUES (NULL, 'papas', 'barcel');
-            $sentenciaSQL= $conexion->prepare("INSERT INTO productos (nombre, imagen) VALUES (:nombre,:imagen);");
-            $sentenciaSQL->bindParam(':nombre',$txtNombre);
-
-            $fecha= new DateTime();
-            $nombreArchivo=($txtImagen!="")?$fecha->getTimestamp()."_".$_FILES["txtImagen"]["name"]:"imagen.jpg";
-
-            $tmpImagen=$_FILES["txtImagen"]["tmp_name"];
-
-            if($tmpImagen!=""){
-
-                move_uploaded_file($tmpImagen,"../../img/".$nombreArchivo);
-            }
-
-            $sentenciaSQL->bindParam(':imagen',$txtImagen);
-            $sentenciaSQL->execute();
-
-            header("Location:productos.php");
-
-            
-            break;
-
-         case"Modificar":
-
-            $sentenciaSQL= $conexion->prepare("UPDATE productos SET nombre=:nombre WHERE id=:id");
-            $sentenciaSQL->bindParam(':nombre',$txtNombre);
-            $sentenciaSQL->bindParam(':id',$txtID);
-            $sentenciaSQL->execute();
-            
-            if($txtImagen!=""){
-
-                $fecha= new DateTime();
-                $nombreArchivo=($txtImagen!="")?$fecha->getTimestamp()."_".$_FILES["txtImagen"]["name"]:"imagen.jpg";
-                $tmpImagen=$_FILES["txtImagen"]["tmp_name"];
-
-                move_uploaded_file($tmpImagen,"../../img/".$nombreArchivo);
-
-                $sentenciaSQL= $conexion->prepare("SELECT imagen FROM productos WHERE id=:id");
-                $sentenciaSQL->bindParam(':id',$txtID);
-                $sentenciaSQL->execute();
-                $Productos=$sentenciaSQL->fetch(PDO::FETCH_LAZY);
-
-                if(isset($Productos['imagen']) &&($Productos["imagen"]!="imagen.jpg") ){
-                        if(file_exists("../../img/".$Productos["imagen"])) {
-                            
-                            unlink("../../img/".$Productos["imagen"]);
-                        }
-                }
+<div class="col-md-2">
 
 
+<div class="card">
 
+<img class="card-img-top" src="https://www.w3schools.com/bootstrap4/img_avatar1.png" alt="">
 
-
-                 $sentenciaSQL= $conexion->prepare("UPDATE productos SET imagen=:imagen WHERE id=:id");
-                $sentenciaSQL->bindParam(':imagen',$nombreArchivo);
-                $sentenciaSQL->bindParam(':id',$txtID);
-                $sentenciaSQL->execute();
-                
-            }
-            
-            header("Location:productos.php");
-            
-                break;
-
-        case"Cancelar":
-                header("Location:productos.php");
-                break;
- 
-                case"Seleccionar":
-
-                    $sentenciaSQL= $conexion->prepare("SELECT * FROM productos WHERE id=:id");
-                    $sentenciaSQL->bindParam(':id',$txtID);
-                    $sentenciaSQL->execute();
-                    $Productos=$sentenciaSQL->fetch(PDO::FETCH_LAZY);
-                    $txtNombre=$Productos['nombre'];
-                    $txtImagen=$Productos['imagen'];
-                    
-
-
-                   // echo "Presionado el boton de Seleccionar";
-                        break;
-            case"Borrar":
-                $sentenciaSQL= $conexion->prepare("SELECT imagen FROM productos WHERE id=:id");
-                $sentenciaSQL->bindParam(':id',$txtID);
-                $sentenciaSQL->execute();
-                $Productos=$sentenciaSQL->fetch(PDO::FETCH_LAZY);
-
-                if(isset($Productos['imagen']) &&($Productos["imagen"]!="imagen.jpg") ){
-                        if(file_exists("../../img/".$Productos["imagen"])) {
-                            
-                            unlink("../../img/".$Productos["imagen"]);
-                        }
-                }
-
-                
-                $sentenciaSQL= $conexion->prepare("DELETE  FROM productos WHERE id=:id");
-                $sentenciaSQL->bindParam(':id',$txtID);
-                $sentenciaSQL->execute();
-
-                header("Location:productos.php");
-                 
-
-               // echo "Presionado el boton de Borrar";
-                                break;
-}
-
-$sentenciaSQL= $conexion->prepare("SELECT * FROM productos");
-$sentenciaSQL->execute();
-$listaProductos=$sentenciaSQL->fetchAll(PDO::FETCH_ASSOC);
-
-
-
-?>
-
-<div class="col-md-5">
-
-    <div class="card">
-        <div class="card-header">
-            DATOS PRODUCTO
-        </div>
-
-        <div class="card-body">
-           
-    <form method="POST" enctype="multipart/form-data" >
-
-<div class = "form-group">
-<label for="txtID">ID:</label>
-<input type="text" required readonly class="form-control" value="<?php echo $txtID; ?>" name="txtID" id="txtID"  placeholder="ID">
+<div class="card-body">
+    <h4 class="card-title">Producto</h4>
+    <a name="" id="" class="btn btn-primary" href="https://goalkicker.com/" role="button">Ver más</a>
 </div>
 
-<div class = "form-group">
-<label for="txtNombre">Nombre:</label>
-<input type="text" required class="form-control" value="<?php echo $txtNombre; ?>" name="txtNombre" id="txtNombre"  placeholder="NOMBRE">
 </div>
 
-<div class = "form-group">
-<label for="txtNombre">Imagen:</label>
+</div>
+<div class="col-md-2">
+<div class="card">
 
-<br/>
+<img class="card-img-top" src="https://www.w3schools.com/bootstrap4/img_avatar2.png" alt="">
 
-<?php if($txtImagen!=""){ ?>
+<div class="card-body">
+    <h4 class="card-title">Producto 1</h4>
+    <a name="" id="" class="btn btn-primary" href="https://goalkicker.com/" role="button">Ver más</a>
+</div>
 
+</div>
 
-    <img class="img-thumbnail rounded" src="../../img/<?php echo $txtImagen?>" width="50" alt="" srcset="">
-
-<?php } ?>
-<input type="file"  class="form-control" name="txtImagen" id="txtImagen"  placeholder="NOMBRE">
 </div>
 
 
+<div class="col-md-2">
+<div class="card">
 
-<div class="btn-group" role="group" aria-label="">
-    <button type="submit" name="accion"    value="Agregar" class="btn btn-success">Agregar</button>
-    <button type="submit" name="accion"     value="Modificar" class="btn btn-warning">Modificar</button>
-    <button type="submit" name="accion"     value="Cancelar" class="btn btn-info">Cancelar</button>
+<img class="card-img-top" src="https://www.w3schools.com/bootstrap4/img_avatar1.png" alt="">
+
+<div class="card-body">
+    <h4 class="card-title">Producto 2</h4>
+    <a name="" id="" class="btn btn-primary" href="https://goalkicker.com/" role="button">Ver más</a>
+</div>
+
+</div>
+
 </div>
 
 
-</form>
-        </div>
-     
-    </div>
+<div class="col-md-2">
+<div class="card">
 
+<img class="card-img-top" src="https://www.w3schools.com/bootstrap4/img_avatar1.png" alt="">
 
-    
+<div class="card-body">
+    <h4 class="card-title">Producto 3</h4>
+    <a name="" id="" class="btn btn-primary" href="https://goalkicker.com/" role="button">Ver más</a>
 </div>
-<div class="col-md-5">
-    <table class="table table-bordered">
-        <thead>
-            <tr>
-                <th>ID</th>
-                <th>Nombre</th>
-                <th>Imagen</th>
-                <th>Acciones</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php foreach($listaProductos as $producto) { ?>
-            <tr>
-                <td><?php echo $producto['id']; ?></td>
-                <td><?php echo $producto['nombre']; ?></td>
-                <td>
 
-                   <img class="img-thumbnail rounded"  src="../../img/<?php echo $producto['imagen']; ?>" width="50" alt="" srcset="">
+</div>
 
+</div>
 
-               
-                
-                </td>
+<div class="col-md-2">
+<div class="card">
 
-                <td>
- 
-                <form method="post">
+<img class="card-img-top" src="https://www.w3schools.com/bootstrap4/img_avatar1.png" alt="">
 
-                <input type="hidden" name="txtID" id="txtID" value="<?php echo $producto['id']; ?>" />
+<div class="card-body">
+    <h4 class="card-title">Producto 4</h4>
+    <a name="" id="" class="btn btn-primary" href="https://goalkicker.com/" role="button">Ver más</a>
+</div>
 
-                <input type="submit" name="accion" value="Seleccionar" class="btn btn-primary" />
+</div>
 
-                <input type="submit" name="accion" value="Borrar" class="btn btn-danger" />
-
-
-                </form>
-            
-            </td>
-
-            </tr>
-            <?php } ?> 
-        </tbody>
-    </table>
-    
 </div>
 
 
-<?php include("../template/pie.php"); ?>
+   
+
+<?php include("template/pie.php"); ?>
